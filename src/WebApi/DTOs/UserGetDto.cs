5f2f570236e042345ce8db14f26a9078223be0d8
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2021-05-02 00:19:21 +0500

    api: return password field to UserGetDto

--- src/WebApi/DTOs/UserGetDto.cs
+++ src/WebApi/DTOs/UserGetDto.cs
@@ -6,6 +6,7 @@ namespace WebApi.DTOs
     {
         public int Id { get; set; }
         public string Login { get; set; }
+        public string Password { get; set; }
         public DateTime CreatedDate { get; set; }
         public UserGroupGetDto Group { get; set; }
         public UserStateGetDto State { get; set; }
