9ead466b7cfcd5940170488732c496faddca5d32
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2021-05-01 21:22:22 +0500

    feat: remove Code properties from UserGroup and UserState DTOs

--- src/WebApi/DTOs/UserGroupGetDto.cs
+++ src/WebApi/DTOs/UserGroupGetDto.cs
@@ -1,11 +1,8 @@
-using Models.Enums;
-
 namespace WebApi.DTOs
 {
     public class UserGroupGetDto
     {
         public int Id { get; set; }
-        public UserGroupCode Code { get; set; }
         public string Description { get; set; }
     }
 }
--- src/WebApi/DTOs/UserStateGetDto.cs
+++ src/WebApi/DTOs/UserStateGetDto.cs
@@ -1,11 +1,8 @@
-using Models.Enums;
-
 namespace WebApi.DTOs
 {
     public class UserStateGetDto
     {
         public int Id { get; set; }
-        public UserStateCode Code { get; set; }
         public string Description { get; set; }
     }
 }
